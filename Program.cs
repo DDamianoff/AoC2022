c4bd972b2a3c1ec9ffcb9747a9f800c470daf7d0
Dino Damianoff <75574873+DDamianoff@users.noreply.github.com>
2022-12-01 18:23:12 -0300

    C# / LINQ solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var rawValueList = File.ReadAllText("./day_one.txt");
+
+var parsedElves = rawValueList
+    .Split("\n\n")
+    .Select(e => 
+        e.Split("\n"))
+    .Select(elf => 
+        elf
+            .Select(int.Parse)
+            .Sum())
+    .ToList();
+
+parsedElves
+    .Max()
+    .Display("Elf with most calories");
+
+parsedElves
+    .Order()
+    .TakeLast(3)
+    .Sum()
+    .Display("Sum of top three elves with most calories");
+
+internal static class Utils
+{
+    public static void Display(this object finalValue, string title) 
+        => Console.WriteLine($"{title}: {finalValue}");
+}
