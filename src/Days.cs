157ee11153fb234a6cb271635d4fff938ae3d0be
Dino Damianoff <75574873+DDamianoff@users.noreply.github.com>
2022-12-02 17:18:46 -0300

    Day two (first part)

--- src/Days.cs
+++ src/Days.cs
@@ -27,6 +27,33 @@ public static class Days
 
     public static void DayTwo()
     {
+        var input = File
+            .ReadAllText("./inputs/day_two.txt")
+            .Split("\n");
+        // this should be an enum.
+        var match = new
+        {
+            Win = 6,
+            Draw = 3,
+            Loose = 0
+        };
         
+        var bonus = new Dictionary<char, int>()
+        {
+            ['X'] = 1,
+            ['Y'] = 2,
+            ['Z'] = 3
+        };
+        
+        input
+            .Select(strategy => strategy switch
+            {
+                "A Y" or "B Z" or "C X" => match.Win,
+                "A X" or "B Y" or "C Z" => match.Draw,
+                "A Z" or "B X" or "C Y" => match.Loose,
+                _ => throw new Exception("Shouldn't happen but makes the Roslyn happy")
+            } + bonus[strategy[2]])
+            .Sum()
+            .Display("Total (first part)");
     }
 }
